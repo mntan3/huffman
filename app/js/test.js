d4435cf9138e26a1beb3dcd48fb7f5c9c2ce0a20
Michelle Tan <mtan123456789@gmail.com>
2017-12-31 02:53:54 -0500

    Added database schema

--- app/js/test.js
+++ app/js/test.js
@@ -1,6 +1,3 @@
 var sentiment = require('sentiment');
-var r1 = sentiment('Cats are stupid');
-console.log(r1);
 
-var r2 = sentiment('Cats are totally amazing');
-console.log(r2);
+module.exports = sentiment;
